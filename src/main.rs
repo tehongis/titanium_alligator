bbf5df9c7c7f6993c19b0b195c12649ff94b3584
Tero Hongisto <tero.hongisto@kpmg.fi>
2023-08-18 08:00:03 +0300

    Added graphics

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,27 @@ extern crate sdl2;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
+use std::path::Path;
 use std::time::Duration;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
         .unwrap();
-
+        
     let mut canvas = window.into_canvas().build().unwrap();
+
+    let texture_creator = canvas.texture_creator();
+    Pathlet pngpath = new("gfx/colored_transparent_packed.png")
+    let texture = texture_creator.load_texture(pngpath)?;
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
